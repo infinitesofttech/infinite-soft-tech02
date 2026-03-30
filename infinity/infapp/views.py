63e8fd05060efa3cd178825b56866e1d8071eeb4
infinity3tech-dev <infinity3tech@gmail.com>
2026-03-30 18:56:10 +0530

    30 march

---
+++ infinity/infapp/views.py
@@ -0,0 +1,226 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+
+def page_404(request):
+    return render(request, "404.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog_grid_2(request):
+    return render(request, "blog-grid-2.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def faq(request):
+    return render(request, "faq.html")
+
+
+def gallery_filter(request):
+    return render(request, "gallery-filter.html")
+
+
+def industries_details(request):
+    return render(request, "industries-details.html")
+
+
+def industries(request):
+    return render(request, "industries.html")
+
+
+def portfolio(request):
+    return render(request, "portfolio.html")
+
+
+def product_details(request):
+    return render(request, "product-details.html")
+
+
+def product(request):
+    return render(request, "product.html")
+
+
+def project_details(request):
+    return render(request, "project-details.html")
+
+
+def projects(request):
+    return render(request, "projects.html")
+
+
+def service_details(request):
+    return render(request, "service-details.html")
+
+
+def services_2(request):
+    return render(request, "services-2.html")
+
+
+def services_carousel_3(request):
+    return render(request, "services-carousel-3.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def team_details(request):
+    return render(request, "team-details.html")
+
+
+def team(request):
+    return render(request, "team.html")
+
+
+def technology_android(request):
+    return render(request, "technology-android.html")
+
+
+def technology_angular(request):
+    return render(request, "technology-angular.html")
+
+
+def technology_appium(request):
+    return render(request, "technology-appium.html")
+
+
+def technology_aws(request):
+    return render(request, "technology-aws.html")
+
+
+def technology_azure(request):
+    return render(request, "technology-azure.html")
+
+
+def technology_backend(request):
+    return render(request, "technology-backend.html")
+
+
+def technology_cloud_devops(request):
+    return render(request, "technology-cloud-devops.html")
+
+
+def technology_database(request):
+    return render(request, "technology-database.html")
+
+
+def technology_details(request):
+    return render(request, "technology-details.html")
+
+
+def technology_dotnet(request):
+    return render(request, "technology-dotnet.html")
+
+
+def technology_flutter(request):
+    return render(request, "technology-flutter.html")
+
+
+def technology_frontend(request):
+    return render(request, "technology-frontend.html")
+
+
+def technology_gcp(request):
+    return render(request, "technology-gcp.html")
+
+
+def technology_ios(request):
+    return render(request, "technology-ios.html")
+
+
+def technology_javascript(request):
+    return render(request, "technology-javascript.html")
+
+
+def technology_magento(request):
+    return render(request, "technology-magento.html")
+
+
+def technology_mobile(request):
+    return render(request, "technology-mobile.html")
+
+
+def technology_mongodb(request):
+    return render(request, "technology-mongodb.html")
+
+
+def technology_mysql(request):
+    return render(request, "technology-mysql.html")
+
+
+def technology_nodejs(request):
+    return render(request, "technology-nodejs.html")
+
+
+def technology_oracle(request):
+    return render(request, "technology-oracle.html")
+
+
+def technology_other(request):
+    return render(request, "technology-other.html")
+
+
+def technology_php_laravel(request):
+    return render(request, "technology-php-laravel.html")
+
+
+def technology_postgresql(request):
+    return render(request, "technology-postgresql.html")
+
+
+def technology_postman(request):
+    return render(request, "technology-postman.html")
+
+
+def technology_python_django(request):
+    return render(request, "technology-python-django.html")
+
+
+def technology_react_native(request):
+    return render(request, "technology-react-native.html")
+
+
+def technology_react(request):
+    return render(request, "technology-react.html")
+
+
+def technology_selenium(request):
+    return render(request, "technology-selenium.html")
+
+
+def technology_shopify(request):
+    return render(request, "technology-shopify.html")
+
+
+def technology_spring_boot(request):
+    return render(request, "technology-spring-boot.html")
+
+
+def technology_testing(request):
+    return render(request, "technology-testing.html")
+
+
+def technology_themeforest(request):
+    return render(request, "technology-themeforest.html")
+
+
+def technology_xamarin(request):
+    return render(request, "technology-xamarin.html")
+
+
+def technology(request):
+    return render(request, "technology.html")
+
+
+def testimonials(request):
+    return render(request, "testimonials.html")
